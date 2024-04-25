4ddb701c6d35fa5040929bd8be96c54e23dd020d
Radio Benzene <45720746+radiobenzene@users.noreply.github.com>
2024-04-25 09:40:54 +0100

    Add files via upload

---
+++ QMetricFunctions.py
@@ -0,0 +1,113 @@
+import numpy as np
+from skimage.util import view_as_blocks
+import cv2
+
+'''
+Function to calculate local metric
+'''
+
+def calculateLocalMetric(patch):
+    # Calculate gradient
+    gx, gy = np.gradient(patch)
+
+    gx = gx.ravel()
+    gy = gy.ravel()
+    G = np.column_stack((gx, gy))
+
+    # Calculate SVD values
+    svd_value = np.linalg.svd(G, compute_uv=False)
+
+    s1 = svd_value[0]
+    s2 = svd_value[1]
+
+    R = (s1 - s2) / (s1 + s2 + 1e-6)
+
+    q_metric = s1 * R
+
+    return q_metric
+'''
+Function to calculate threshold
+'''
+#@njit
+def getThreshold(delta, N):
+    power_val = (N ** 2) - 1
+    k_val = delta ** (1.0 / power_val)
+
+    numerator = np.sqrt(1 - k_val)
+    denominator = np.sqrt(k_val + 1)
+
+    tau = numerator / denominator
+
+    return tau
+
+'''
+Function to calculate local coherence
+'''
+
+def calculateLocalCoherence(patch):
+    # Calculate gradient
+    gx, gy = np.gradient(patch)
+
+    gx = gx.ravel()
+    gy = gy.ravel()
+    G = np.column_stack((gx, gy))
+
+    # Calculate SVD values
+    svd_value = np.linalg.svd(np.dot(G.T, G), compute_uv=False)
+
+    s1 = svd_value[0]
+    s2 = svd_value[1]
+
+    R = (s1 - s2) / (s1 + s2 + np.finfo(np.float64).eps)
+
+    coherence = R
+
+    return coherence
+
+'''
+Function to calculate metric Q
+'''
+def calculateQ(img, delta):
+    # Initializing patch size as 8x8
+    patch_size = (8, 8)
+
+    # Convert image to double
+    img = (img/255.0).astype(np.float64)
+
+    # Divide image into patches
+    patches = view_as_blocks(img, patch_size)
+
+    # Calculating dimensions of the image
+    h, w = img.shape
+
+    total_patches = (h // patch_size[0]) * (w // patch_size[1])
+
+    # Initialize local coherence
+    local_coherence = np.zeros((patches.shape[0], patches.shape[1]))
+
+    # Generate threshold based on formula
+    threshold = getThreshold(delta, patch_size[0])
+
+    # Calculate local coherence based on formula
+    for i in range(patches.shape[0]):
+        for j in range(patches.shape[1]):
+            local_coherence[i, j] = calculateLocalCoherence(patches[i, j])
+
+    # Define type of patches - anisotropic or isotropic
+    anisotropic_patches = (local_coherence.flatten() > threshold)
+
+    # Initialize local metric for each patch
+    local_metric = np.zeros(anisotropic_patches.shape)
+
+    # Calculate local metric for each patch
+    for i in range(anisotropic_patches.shape[0]):
+        if anisotropic_patches[i]:
+            local_metric[i] = calculateLocalMetric(patches.reshape(-1, patch_size[0], patch_size[1])[i])
+
+    summed_value = local_metric.sum()
+
+    local_metric = local_metric.reshape((patches.shape[0], patches.shape[1]))
+
+    q_metric = summed_value.sum() / patches.shape[0] #16
+
+    return q_metric
