9e447712646f36980923e3e79df5bee0170a68e2
radiobenzene <aurangabadkaruditangshu@gmail.com>
2026-02-10 12:30:52 +0000

    upd: update image IO with cv2

--- sampleUsage.py
+++ sampleUsage.py
@@ -5,15 +5,13 @@ from PIL import Image
 # Specifying image name
 IMG_NAME = '/media/udit/Backup_Files/PythonQ/testImages/I_GT.png'
 
-img = Image.open(IMG_NAME)
-arr = np.array(img)
+# img = Image.open(IMG_NAME)
+# arr = np.array(img)
 
-print("PIL mode:", img.mode)
-print("dtype:", arr.dtype, "shape:", arr.shape)
-print("min/max:", arr.min(), arr.max())
+img = cv2.imread(IMG_NAME, cv2.IMREAD_GRAYSCALE)
 
 
 # Measure Q
-#Q = calculateQ(arr, 0.001)
+Q = calculateQ(img, 0.001)
 
-#print('Q = ', Q)
+print('Q = ', Q)
